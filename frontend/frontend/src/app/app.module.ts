8d20ca5b305f83c3a302925b622b6cb443a29e3b
Pedro Rhamon <pedrorhamon16@gmail.com>
2021-05-28 11:58:14 -0300

    criando componente read

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import {MatInputModule} from '@angular/material/input'
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 
 
@@ -35,7 +36,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
